df0d69eaebd9dfbcce44be94dad2644ef5091dd7
Kevin Mulvey <kmulvey@linux.com>
2015-02-02 18:41:07 -0500

    template working

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+)
+
+type Framework struct {
+	Name string
+}
+
+func main() {
+	/*
+		s := spin.New()
+		for i := 0; i < 30; i++ {
+			fmt.Printf("\r  \033[36mcomputing\033[m %s ", s.Next())
+			time.Sleep(100 * time.Millisecond)
+		}
+	*/
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter project name: ")
+	nameArr, _, _ := reader.ReadLine()
+	name := string(nameArr)
+	os.Mkdir("."+string(filepath.Separator)+name, 0755)
+
+	p := Framework{name}
+	//tmpl, err := template.ParseFiles("templates/gin/gin")
+	tmpl, err := template.ParseFiles("templates/gin/server.go")
+	fmt.Printf("%+v", tmpl)
+	_ = tmpl.ExecuteTemplate(os.Stdout, tmpl.Name(), p)
+	if err != nil {
+		panic(err)
+	}
+	/*
+		// open output file
+		fo, err := os.Create("test/server.go")
+		if err != nil {
+			panic(err)
+		}
+		// close fo on exit and check for its returned error
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
+	*/
+}

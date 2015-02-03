fbbb7ee5c09a64b4f9abd9c30fc4373aa37b19fd
Kevin Mulvey <kmulvey@linux.com>
2015-02-03 07:52:26 -0500

    process json

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
-	"html/template"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
-
-type Framework struct {
-	Name       string
-	Middleware []string
-}
 
 func main() {
 	/*
@@ -21,32 +17,46 @@ func main() {
 			time.Sleep(100 * time.Millisecond)
 		}
 	*/
+	// load configs
+	var frameworks []Framework
+	getConfigs(&frameworks)
 
+	// talk to the user
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter project name: ")
 	nameArr, _, _ := reader.ReadLine()
 	name := string(nameArr)
 	os.Mkdir("."+string(filepath.Separator)+name, 0755)
 
-	f := Framework{name, make([]string, 0)}
+	/*
+		f := Framework{name, make([]string, 1)}
 
-	// open output file
-	fo, err := os.Create("test/server.go")
-	if err != nil {
-		panic(err)
-	}
-	// close fo on exit and check for its returned error
-	defer func() {
-		if err := fo.Close(); err != nil {
+		// open output file
+		fo, err := os.Create("test/server.go")
+		if err != nil {
 			panic(err)
 		}
-	}()
+		// close fo on exit and check for its returned error
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
 
-	// process template
-	tmpl, err := template.ParseFiles("templates/gin/server.go")
-	_ = tmpl.ExecuteTemplate(fo, tmpl.Name(), f)
-	if err != nil {
-		panic(err)
+		// process template
+		tmpl, err := template.ParseFiles("templates/gin/server.go")
+		_ = tmpl.ExecuteTemplate(fo, tmpl.Name(), f)
+		if err != nil {
+			panic(err)
+		}
+	*/
+}
+
+func getConfigs(frameworks *[]Framework) {
+	file, e := ioutil.ReadFile("./frameworks.json")
+	if e != nil {
+		fmt.Printf("Unable to open config file: %v", e)
+		os.Exit(1)
 	}
-
+	json.Unmarshal(file, &frameworks)
 }
